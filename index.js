825a71280fe7ddbe7ba2c858aada8b4644f43238
pigeon.js <rishivyas2424@gmail.com>
2020-09-18 08:11:42 +0530

    hey

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const express = require('express') ;
 const app = express() ;
 const port = process.env.PORT || '8000' ;
 
+//route
 app.get('/', (req, res) => {
     res.status(200).send("Hey..How you doing.!?") ;
 })
 
+
+// listening port
 app.listen(port, () => {
     console.log("Yeah..i am listening.") ;
 } )
